502fee1a80b1b585d83e6acab46683530767b082
yang20002020 <2628636987@qq.com>
2023-06-09 16:35:01 +0800

    bean标签scope属性

---
+++ spring_ioc_1/src/main/java/com/fuyu/Demo/UserDaoDemo.java
@@ -0,0 +1,19 @@
+package com.fuyu.Demo;
+
+import com.fuyu.dao.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/*
+客户端代码
+ */
+public class UserDaoDemo {
+    public static void main(String[] args){
+        ApplicationContext app=new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao=(UserDao) app.getBean("userDao");
+        userDao.save();
+        System.out.println(userDao);
+        UserDao userDao1=(UserDao) app.getBean("userDao");
+        System.out.println(userDao1);
+    }
+}
---
+++ spring_ioc_1/src/main/java/com/fuyu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.fuyu.dao.impl;
+
+import com.fuyu.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void save() {
+        System.out.println("save running......");
+    }
+}
