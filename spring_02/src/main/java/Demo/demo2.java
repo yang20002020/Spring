5f729caf0fe15a33cbe5124f1db58324f59c621d
yang20002020 <2628636987@qq.com>
2023-06-10 11:41:12 +0800

    IoC入门案例

---
+++ spring_02/src/main/java/Demo/demo2.java
@@ -0,0 +1,19 @@
+package Demo;
+
+import com.fuyu.dao.BookDao;
+import com.fuyu.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class demo2 {
+    public static void main(String[] args){
+        //3.获得IoC容器
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //4.获取bean
+        BookService  boookService=(BookService)ctx.getBean("bookService123");
+        boookService.save();
+        System.out.println("********************************");
+        BookDao bookDao=(BookDao) ctx.getBean("bookDao123");
+        bookDao.save();
+    }
+}
