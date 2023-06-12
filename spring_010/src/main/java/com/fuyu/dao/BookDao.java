64163018e3057f15809deeb90f5dfcc0f9401de2
yang20002020 <2628636987@qq.com>
2023-06-12 09:03:59 +0800

    构造器注入---引用类型

---
+++ spring_010/src/main/java/com/fuyu/dao/BookDao.java
@@ -0,0 +1,6 @@
+package com.fuyu.dao;
+
+public interface BookDao {
+
+    public void save();
+}
---
+++ spring_010/src/main/java/com/fuyu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package com.fuyu.dao.impl;
+
+import com.fuyu.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void save() {
+        System.out.println("User dao save.......");
+    }
+}
