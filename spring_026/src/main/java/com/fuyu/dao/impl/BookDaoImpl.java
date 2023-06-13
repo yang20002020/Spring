96af1e87206eff5b91c59579c9047eaee71f386d
yang20002020 <2628636987@qq.com>
2023-06-13 16:25:10 +0800

    简单类型自动配值

---
+++ spring_026/src/main/java/com/fuyu/dao/impl/BookDaoImpl.java
@@ -0,0 +1,16 @@
+package com.fuyu.dao.impl;
+
+import com.fuyu.dao.BookDao;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository("bookDao")
+public class BookDaoImpl implements BookDao {
+    @Value("ityangzhang")
+    private  String  name;
+    @Override
+    public void save() {
+        System.out.println("book dao save......."+name);
+    }
+
+}
