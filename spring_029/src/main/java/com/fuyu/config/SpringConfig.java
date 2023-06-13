50e3d392926c41586403c88a18c6afc644a58a8c
yang20002020 <2628636987@qq.com>
2023-06-13 20:01:33 +0800

    第三方bean管理

---
+++ spring_029/src/main/java/com/fuyu/config/SpringConfig.java
@@ -0,0 +1,11 @@
+package com.fuyu.config;
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.fuyu.config")
+public class SpringConfig {
+}
