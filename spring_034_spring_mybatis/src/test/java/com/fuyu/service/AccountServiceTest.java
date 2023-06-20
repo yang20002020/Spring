ee367b66020b812c6236b286b9f004b47b55058e
yang20002020 <2628636987@qq.com>
2023-06-20 16:17:56 +0800

    spring 整合JUnit

---
+++ spring_034_spring_mybatis/src/test/java/com/fuyu/service/AccountServiceTest.java
@@ -0,0 +1,31 @@
+package com.fuyu.service;
+import com.fuyu.config.SpringConfig;
+import com.fuyu.domain.Account;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class AccountServiceTest {
+
+    @Autowired
+    private AccountService accountService;
+    @Test
+    public void testFindById(){
+        Account byId = accountService.findById(1);
+        System.out.println(byId);
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Account> all = accountService.findAll();
+        System.out.println(all);
+    }
+
+}
