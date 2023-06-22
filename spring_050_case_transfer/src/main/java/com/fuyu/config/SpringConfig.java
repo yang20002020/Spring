79f23eec3fe025c4d8d3570ac575e25bcd096599
yang20002020 <2628636987@qq.com>
2023-06-22 22:13:00 +0800

    案例 银行转账  spring不添加事务

---
+++ spring_050_case_transfer/src/main/java/com/fuyu/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.fuyu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.fuyu")
+@PropertySource("classpath:jdbc.properties")
+@Import({JdbcConfig.class,MybatisConfig.class})
+public class SpringConfig {
+}
---
+++ spring_050_case_transfer/src/main/java/com/fuyu/service/impl/AccountServiceImpl.java
@@ -0,0 +1,19 @@
+package com.fuyu.service.impl;
+import com.fuyu.dao.AccountDao;
+import com.fuyu.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    public void transfer(String out,String in ,Double money) {
+        accountDao.outMoney(out,money);
+        int i = 1/0;
+        accountDao.inMoney(in,money);
+    }
+
+}
