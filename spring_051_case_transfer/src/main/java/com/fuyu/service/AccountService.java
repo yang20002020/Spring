b06f0783eac3ca26b3ca213fed2d7b8a65bb2b16
yang20002020 <2628636987@qq.com>
2023-06-22 23:40:17 +0800

    案例 银行转账  spring事务

---
+++ spring_051_case_transfer/src/main/java/com/fuyu/service/AccountService.java
@@ -0,0 +1,17 @@
+package com.fuyu.service;
+
+
+import org.springframework.transaction.annotation.Transactional;
+
+//转账接口
+public interface AccountService {
+    /**
+     * 转账操作
+     * @param out 传出方
+     * @param in 转入方
+     * @param money 金额
+     */
+    //配置当前接口方法具有事务
+    @Transactional
+    public void transfer(String out,String in ,Double money) ;
+}
