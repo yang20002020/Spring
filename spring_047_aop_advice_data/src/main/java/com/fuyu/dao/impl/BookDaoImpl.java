8b1f7c3b9b911400559002ef612e2b7ac7ce262b
yang20002020 <2628636987@qq.com>
2023-06-22 16:48:51 +0800

    返回后通知获取切入点方法的参数

---
+++ spring_047_aop_advice_data/src/main/java/com/fuyu/dao/impl/BookDaoImpl.java
@@ -0,0 +1,14 @@
+package com.fuyu.dao.impl;
+
+import com.fuyu.dao.BookDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookDaoImpl implements BookDao {
+
+    public String findName(int id,String password) {
+        System.out.println("findName 函数"+"id:"+id+ " " +"password:"+password);
+
+        return "findName函数返回值itcast";
+    }
+}
