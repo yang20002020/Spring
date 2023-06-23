f45bca49589098ad049322172ae1341499cfc354
yang20002020 <2628636987@qq.com>
2023-06-23 15:03:03 +0800

    案例  银行转账，日志没有登记记录

---
+++ spring_053_case_transfer_log/src/main/java/com/itheima/service/LogService.java
@@ -0,0 +1,8 @@
+package com.itheima.service;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface LogService {
+    //propagation设置事务属性：传播行为设置为当前操作需要新事务
+    @Transactional
+    void log(String out, String in, Double money);
+}
